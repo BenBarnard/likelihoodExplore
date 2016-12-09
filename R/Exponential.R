4bdf7e4453444122487ed7e267eb6a42582da3cb
BenBarnard <ben_barnard@baylor.edu>
2016-12-09 11:54:46 -0600

    added flik and rlik

--- R/Exponential.R
+++ R/Exponential.R
@@ -8,12 +8,21 @@
 #' @export
 #'
 #' @examples likexp(rexp(4, 2), 2)
-likexp <- function(data, rate, log = TRUE){
-  n <- length(data)
+fexplik <- function(data, rate, log = TRUE){
 
-  if(log == TRUE){
-    n * log(rate) + (- rate * sum(data))
-  }else{
-    (rate ^ n) * exp(-rate * sum(data))
-  }
+}
+
+
+#' Exponential Likelihood Function
+#'
+#' @param data vector of data points
+#' @param rate rate of the exponential where the mean is 1 / rate
+#' @param log logical; if TRUE, output is the log likelihood
+#'
+#' @return
+#' @export
+#'
+#' @examples likexp(rexp(4, 2), 2)
+rexplik <- function(data, rate, log = TRUE){
+
 }
