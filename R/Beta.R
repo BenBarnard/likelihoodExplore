569b06f708267ebf617fdc35470770cc576fb157
BenBarnard <ben_barnard@baylor.edu>
2016-12-09 11:54:05 -0600

    added flik and rlik

--- R/Beta.R
+++ R/Beta.R
@@ -9,6 +9,22 @@
 #' @export
 #'
 #' @examples likbeta(rbeta(4, 0.5, 0.5), 0.5, 0.5)
-likbeta <- function(data, a, b, log = TRUE){
+fbetalik <- function(data, a, b, log = TRUE){
+
+}
+
+
+#' Beta Likelihood Function
+#'
+#' @param data vector of data points
+#' @param a shape parameter alpha
+#' @param b shape parameter beta
+#' @param log logical; if TRUE, output is the log likelihood
+#'
+#' @return
+#' @export
+#'
+#' @examples likbeta(rbeta(4, 0.5, 0.5), 0.5, 0.5)
+rbetalik <- function(data, a, b, log = TRUE){
 
 }
