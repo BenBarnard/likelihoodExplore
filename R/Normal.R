206216658078014d0ee930fbcc5026acb0d51d2b
BenBarnard <ben_barnard@baylor.edu>
2016-12-09 11:21:23 -0600

    added normal script file

---
+++ R/Normal.R
@@ -0,0 +1,27 @@
+#' Normal Likelihood Function
+#'
+#' @param data vector of data points
+#' @param log logical; if TRUE, output is the log likelihood
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fnormlik <- function(data, ..., log = TRUE){
+
+}
+
+#' Normal Likelihood Function
+#'
+#' @param data vector of data points
+#' @param log logical; if TRUE, output is the log likelihood
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+rnormlik <- function(data, ..., log = TRUE){
+
+}
