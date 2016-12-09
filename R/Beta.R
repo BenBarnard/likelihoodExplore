a26b35e491d9884dbb3e55eaf8401eaba47bce3d
BenBarnard <ben_barnard@baylor.edu>
2016-12-09 11:21:35 -0600

    added beta script file

---
+++ R/Beta.R
@@ -0,0 +1,14 @@
+#' Beta Likelihood Function
+#'
+#' @param data vector of data points
+#' @param a shape parameter alpha
+#' @param b shape parameter beta
+#' @param log logical; if TRUE, output is the log likelihood
+#'
+#' @return
+#' @export
+#'
+#' @examples likbeta(rbeta(4, 0.5, 0.5), 0.5, 0.5)
+likbeta <- function(data, a, b, log = TRUE){
+
+}
