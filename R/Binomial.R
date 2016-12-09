3a06df99d182c3895270de5e7a42ab666c00398e
BenBarnard <ben_barnard@baylor.edu>
2016-12-09 11:54:29 -0600

    added flik and rlik

--- R/Binomial.R
+++ R/Binomial.R
@@ -9,15 +9,22 @@
 #' @export
 #'
 #' @examples likbinom(rbinom(4, 1, .9), 1, .9)
-likbinom <- function(data, size, prob, log = TRUE){
-  n <- length(data)
+fbinomlik <- function(data, size, prob, log = TRUE){
 
-  if(log == TRUE){
-    sum(log(choose(size, data))) +
-      sum(data) * log(prob) +
-      sum(size - data) * log(1 - prob)
-  }else{
-    prod(choose(size, data)) * (prob ^ sum(data)) *
-      ((1 - prob) ^ sum(size - data))
-  }
+}
+
+
+#' Binomial Likelihood Function
+#'
+#' @param data vector of data points
+#' @param size number of bernoulli trials
+#' @param prob probability of success on each trial
+#' @param log logical; if TRUE, output is the log likelihood
+#'
+#' @return
+#' @export
+#'
+#' @examples likbinom(rbinom(4, 1, .9), 1, .9)
+rbinomlik <- function(data, size, prob, log = TRUE){
+
 }
