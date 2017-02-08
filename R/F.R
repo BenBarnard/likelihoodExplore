027e53d6b5e52a5b523ebe62b942c2457e4c8174
BenBarnard <ben_barnard@baylor.edu>
2017-02-07 22:36:27 -0600

    created f likelihood

--- R/F.R
+++ R/F.R
@@ -1,26 +1,10 @@
+source("R/likelihood.R")
 #' F Likelihood Function
 #'
-#' @param data vector of data points
-#' @param df1 degrees of freedom
-#' @param df2 degrees of freedom
-#' @param log logical; if TRUE, output is the log likelihood
-#'
+#' @inheritParams stats::df
+#' @importFrom lazyeval lazy_dots
+#' @importFrom lazyeval lazy_eval
+#' @importFrom plyr llply
 #' @export
 #'
-fflik <- function(data, df1, df2, log = TRUE){
-
-}
-
-
-#' F Likelihood Function
-#'
-#' @param data vector of data points
-#' @param df1 degrees of freedom
-#' @param df2 degrees of freedom
-#' @param log logical; if TRUE, output is the log likelihood
-#'
-#' @export
-#'
-rflik <- function(data, df1, df2, log = TRUE){
-
-}
+likf <- likelihood(df)
