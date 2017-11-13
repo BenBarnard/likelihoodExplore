fe61f8b242f2c32302949e1f2a0a0b1ccd693a17
BenBarnard <ben_barnard@baylor.edu>
2017-11-13 16:25:27 -0600

    documentation and site rebuild

--- R/likelihoodExplore-package.R
+++ R/likelihoodExplore-package.R
@@ -1,9 +1,9 @@
 #' @title Likelihood Exploration
 #'
-#' @description A package of likelihood functions and a function that creates
-#' likelihood functions from density functions. The functions are meant to aid
-#' in education of likelihood based methods. Included shiny app leverages
-#' likelihood functions to explore likelihood based methods.
+#' @description A package of likelihood functions as defined by Fisher
+#' <doi:10.1098/rsta.1922.0009> and a function that creates likelihood
+#' functions from density functions. The functions are meant to aid in
+#' education of likelihood based methods.
 #' @docType package
 #' @name likelihoodExplore
 NULL
